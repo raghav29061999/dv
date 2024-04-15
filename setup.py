93db391271a0757c0efb22ec3f0967ac9d3e716c
RazorEngineer <singhalraghav.59@gmail.com>
2024-04-15 07:47:04 +0530

    setting up repo

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dv',
+    version='1.0.0',
+    author='Raghav Singhal',
+    author_email='singhalraghav.59@gmail.com',
+    description='A package for data visualization',
+    long_description='A package that provides functionality for data visualization using pandas, seaborn, and matplotlib.',
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/dv',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'seaborn',
+        'matplotlib'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
